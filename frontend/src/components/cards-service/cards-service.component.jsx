6236e6a8630a897b3e65047357ab55e84ab39774
grelad <grechenenko@gmail.com>
2020-07-06 22:38:12 +0300

    Add frontend branch

---
+++ frontend/src/components/cards-service/cards-service.component.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export default class CardsService{
+
+    constructor(){}
+
+
+    getCards() {
+        const url = `${API_URL}/api/cards/`;
+        return axios.get(url).then(response => response.data);
+    }
+    getCardsByURL(link){
+        const url = `${API_URL}${link}`;
+        return axios.get(url).then(response => response.data);
+    }
+    getCard(pk) {
+        const url = `${API_URL}/api/cards/${pk}`;
+        return axios.get(url).then(response => response.data);
+    }
+    deleteCard(card){
+        const url = `${API_URL}/api/cards/${card.pk}`;
+        return axios.delete(url);
+    }
+    createCard(card){
+        const url = `${API_URL}/api/cards/`;
+        return axios.post(url,card);
+    }
+    updateCard(card){
+        const url = `${API_URL}/api/cards/${card.pk}`;
+        return axios.put(url,card);
+    }
+}
